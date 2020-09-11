e43ad6ea24b62e9ad8f15081b18051ecd1c67d41
rsemburakkiannan <raghu182@yahoo.com>
2020-09-11 23:19:03 +0530

    clean up

--- ec2/ec2-userdata.sh
+++ ec2/ec2-userdata.sh
@@ -9,8 +9,3 @@ yum install -y httpd.x86_64
 systemctl start httpd.service
 systemctl enable httpd.service
 echo "Hello World from Raghu - Host public IP: $(curl http://checkip.amazonaws.com)" > /var/www/html/index.html
-
-#To get public IP
-curl http://169.254.169.254/latest/meta-data/public-ipv4
-curl http://checkip.amazonaws.com
-
