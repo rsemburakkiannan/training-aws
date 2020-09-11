6aab80a51a7d4228823d5ff26cabc81ee83c4d93
rsemburakkiannan <raghu182@yahoo.com>
2020-09-11 23:37:59 +0530

    Script updates

--- ec2/ec2-userdata.sh
+++ ec2/ec2-userdata.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# get admin privileges
-sudo su
-
-# install httpd (Linux 2 version)
 yum update -y
-yum install -y httpd.x86_64
+yum install -y httpd
 systemctl start httpd.service
 systemctl enable httpd.service
-echo "Hello World from Raghu - Host public IP: $(curl http://checkip.amazonaws.com)" > /var/www/html/index.html
+EC2_AVAIL_ZONE=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
+EC2_IP_ADDR=$(curl http://checkip.amazonaws.com)
+echo "<h1>Hello World from Raghu - Host Public IP: $EC2_IP_ADDR in AZ $EC2_AVAIL_ZONE </h1>" > /var/www/html/index.html
